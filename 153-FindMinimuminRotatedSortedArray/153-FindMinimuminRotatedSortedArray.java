292e204db8fbe3f6bb811ccb7a05bd3bb9c231b4
Ashmitha <ashmithasathish.22@gmail.com>
2026-09-26 11:52:10 +0530

    File updated at 9/26/2026, 11:52:11 AM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
@@ -1,25 +1,51 @@
-// Last updated: 9/26/2026, 11:51:49 AM
-1class Solution {
-2    public int findMin(int[] nums) {
-3
-4        int left = 0;
-5        int right = nums.length - 1;
-6
-7        while (left < right) {
-8
-9            int mid = left + (right - left) / 2;
-10
-11            if (nums[mid] > nums[right]) {
-12                left = mid + 1;
-13            }
-14            else if (nums[mid] < nums[right]) {
-15                right = mid;
-16            }
-17            else {
-18                right--;
-19            }
-20        }
-21
-22        return nums[left];
-23    }
-24}
+// Last updated: 9/26/2026, 11:52:11 AM
+1import java.util.Stack;
+2
+3class MinStack {
+4
+5    Stack<Integer> mainStack;
+6    Stack<Integer> minStack;
+7
+8    public MinStack() {
+9
+10        mainStack = new Stack<>();
+11        minStack = new Stack<>();
+12
+13    }
+14
+15    public void push(int value) {
+16
+17        mainStack.push(value);
+18
+19        if (minStack.isEmpty() || value <= minStack.peek()) {
+20
+21            minStack.push(value);
+22
+23        }
+24
+25    }
+26
+27    public void pop() {
+28
+29        if (mainStack.peek().equals(minStack.peek())) {
+30
+31            minStack.pop();
+32
+33        }
+34
+35        mainStack.pop();
+36
+37    }
+38
+39    public int top() {
+40
+41        return mainStack.peek();
+42
+43    }
+44
+45    public int getMin() {
+46
+47        return minStack.peek();
+48
+49    }
+50}
